0b7d78a3145e0ae2359679dcb2225dddf68fde4c
tekthabhavyasri-glitch <tekthabhavyasri@gmail.com>
2025-10-15 10:33:32 +0530

    Add files via upload

--- LAB5/LAB5P1.java
+++ LAB5/LAB5P1.java
@@ -1,1 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        try{
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println(x/y);
+        }catch(InputMismatchException ie){
+            System.out.println(ie.getClass().getName());
+        }catch(ArithmeticException ae){
+            System.out.println(ae.getClass().getName()+": "+ae.getMessage());
+        }
+    }
+}
