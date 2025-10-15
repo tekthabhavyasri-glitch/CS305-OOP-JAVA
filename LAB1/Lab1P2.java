be2d33a33f3e137159d8858f342e4112158c1b1e
tekthabhavyasri-glitch <tekthabhavyasri@gmail.com>
2025-10-15 11:42:47 +0530

    Add files via upload

---
+++ LAB1/Lab1P2.java
@@ -0,0 +1,8 @@
+class Lab1P2{
+         public static void main(String...args){
+         var a=20;
+         var b=50.15;
+         var c=a+b;
+         System.out.println("Sum is: "+c);
+            }
+ }
